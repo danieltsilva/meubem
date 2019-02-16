c0b47a1ee379bc3516777b917cdab361b3c3d95e
danieltsilva <eugostodequeijadinhadequeijo@gmail.com>
2019-02-16 12:25:34 +1100

    Header component

---
+++ exams/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="ui header">UNIFAE 2015/1.1 [ANTIGUIDADE CLÁSSICA | ROMA]</div>
+    );
+  }
+}
+
+export default Header;
