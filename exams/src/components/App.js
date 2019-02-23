ec7718ad5d9a29bd30a6b90076fea2af07ff754d
danieltsilva <eugostodequeijadinhadequeijo@gmail.com>
2019-02-23 12:18:56 +1100

    Creating the first structure for a basic form for questions

--- exams/src/components/App.js
+++ exams/src/components/App.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
-
-import Header from './Header';
-import Statement from './Statement';
-import Alternatives from './Alternatives';
+  
 import Menu from './Menu';
 import history from '../history';
+import QuestionShow from './questions/QuestionShow';
+import QuestionList from './questions/QuestionList';
+import QuestionCreate from './questions/QuestionCreate';
 
 class App extends React.Component {
- render() {
+  render() {
     return (
       <div className="ui container">
         <Router history={history}>
           <div>
             <Menu />
-            <Header />
-            <Statement />
-            <Alternatives />
+            <Switch>
+              <Route path="/" exact component={QuestionList} />
+              <Route path="/questions/new" exact component={QuestionCreate} />
+              <Route path="/question/:id" exact component={QuestionShow} />
+            </Switch>
           </div>
         </Router>
       </div>
